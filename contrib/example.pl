3c8a6136bedede48a8d5c64d7fe42c6134cbb4f9
Frank Schreiner <schreiner@suse.de>
2017-10-13 08:41:19 +0200

    beautify output in example script

--- contrib/example.pl
+++ contrib/example.pl
@@ -13,10 +13,22 @@ use Net::Docker::Registry::Client;
 my $config = YAML::LoadFile("$FindBin::Bin/../etc/config.yml");
 my $drc = Net::Docker::Registry::Client->new(%$config);
 
+print "\n";
+
 for my $rep (@{$drc->repositories()}) {
+  print "- Found repository $rep\n";
   for my $tag (@{$drc->list_tags($rep)}) {
-    print Dumper($drc->manifests($rep, $tag));
+    print "-- Found tag $tag\n";
+    my $manifest = $drc->manifests($rep, $tag);
+    print "  Name : $manifest->{name}\n";
+    print "  Tag  : $manifest->{tag}\n";
+    print "  Arch : $manifest->{architecture}\n";
+    print "  Layers:\n";
+    foreach my $layer (@{$manifest->{fsLayers}}) {
+      print "   - $layer->{blobSum}\n";
+    }
   }
+  print "\n\n";
 }
 
 
